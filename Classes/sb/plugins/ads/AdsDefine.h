25c4bd463588cc115b840385a9e4e8d1141529d7
gmachinee <gmachinee@superbomb.co>
2021-06-24 18:48:27 +0900

    [deleted] 불필요한 코드 제거

--- Classes/sb/plugins/ads/AdsDefine.h
+++ Classes/sb/plugins/ads/AdsDefine.h
@@ -49,15 +49,13 @@ static inline std::string adTypeToString(const AdType &type) {
 struct AdsConfig {
     // 테스트 모드
     bool testMode;
-    std::vector<std::string> testDevices;
-    
+
     float autoLoadInterval;             // 광고 자동 로드 시간 간격 (단위 : 초)
     
     std::string appId;                  // 애드몹 앱 아이디
     std::string bannerUnitId;
     std::string interstitialUnitId;
     std::string rewardedVideoUnitId;
-    std::map<AdPlatform, std::vector<std::string>> placements;
     
     AdsConfig() : testMode(false), autoLoadInterval(0), appId("") {}
 };
